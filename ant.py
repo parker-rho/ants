fd432d639c320192a56bdf450057d9d40c29120a
Parker-Rho <parkerrho@gmail.com>
2026-03-08 18:09:53 -0400

    Implement pheromone update function

--- ant.py
+++ ant.py
@@ -1,1 +1,8 @@
 # Holds the logic for updating pheromone levels on the edges and the movement of ants through the graph.
+
+def update_pheromone(graph, flow, decay=0.9):
+    '''Updates the pheromone levels on the edges based on the flow of ants and a decay factor.'''
+    for i in graph:
+        for j in graph[i]:
+            graph[i][j] = decay * (graph[i][j] + flow[i][j] + flow[j][i])
+    return graph
