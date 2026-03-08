ba5aeec455b8f29db1105265977516dbdc523885
Aaron Song <ams799@cornell.edu>
2026-03-08 17:41:08 -0400

    Added tests.py with functions that use common shortest-path finding algorithms as a benchmark.

---
+++ tests.py
@@ -0,0 +1,27 @@
+def djikstras(self):
+  """
+  Inputs:
+  Returns:
+  """
+  pass
+
+def astar(self):
+  """
+  Inputs:
+  Returns:
+  """
+  pass
+
+def bfs(self):
+  """
+  Inputs:
+  Returns:
+  """
+  pass
+
+def dfs(self):
+  """
+  Inputs:
+  Returns:
+  """
+  pass
