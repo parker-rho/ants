64d9a7c08c2fe96f4e3f2e068e9b2411277bc7d1
Aaron Nagler <atn45@cornell.edu>
2026-03-22 16:43:32 -0400

    Update ant.py
    fixed update flow and create flow

--- ant.py
+++ ant.py
@@ -1,5 +1,6 @@
 # Holds the logic for updating pheromone levels on the edges and the movement of ants through the graph.
 import graph
+
 def update_pheromone(graph, flow, decay=0.9):
     '''Updates the pheromone levels on the edges based on the flow of ants and a decay factor.'''
     for i in graph:
@@ -9,30 +10,47 @@ def update_pheromone(graph, flow, decay=0.9):
 
 #will have a budget of flow, have that scalar of flow at the source
 #each ant is like a flow
-def create_flow(n, source, base_flow_amount):
-    graph = {}
-    for i in range(n):
-        for j in range(n):
-            if (i,j) == (source[0],source[1]):
-               graph[i][j] = base_flow_amount
+#n = length/width of square graph
+#s = source
+def create_flow(forward, n, s, base_flow_amount):
+    flow = {}
+    for u in range(n):
+        for v in range(n):
+            if (u,v) == s and forward:
+                total_phermones = phermone[(u,v)][(u+1,v)]+phermone[(u,v)][(u-1,v)]+phermone[(u,v)][(u,v+1)]+phermone[(u,v)][(u,v-1)]
+                flow[(u,v)][(u+1,v)] = base_flow_amount*(phermone[(u,v)][(u+1,v)]/total_phermones)
+                flow[(u,v)][(u-1,v)] = base_flow_amount*(phermone[(u,v)][(u-1,v)]/total_phermones)
+                flow[(u,v)][(u,v+1)] = base_flow_amount*(phermone[(u,v)][(u,v+1)]/total_phermones)
+                flow[(u,v)][(u,v-1)] = base_flow_amount*(phermone[(u,v)][(u,v-1)]/total_phermones)
             else:
-               graph[i][j] = 0
+                flow[(u,v)][(u+1,v)] = 0
+                flow[(u,v)][(u-1,v)] = 0
+                flow[(u,v)][(u,v+1)] = 0
+                flow[(u,v)][(u,v-1)] = 0
     return graph
 
 #no leakage
-def update_flow(flow, phermone, n):
+#n = length/width of square graph
+#d = destination (only outputs backwards flow)
+def update_flow(flow, forward, backward, phermone, n, d):
   for u in range(n):
       for v in range(n):
-        f_u = 0
-        p_u = 0
-        for z in range(n):
-            f_u = f_u + flow(z,u)
-            p_u = p_u + phermone(u,z)
-        flow(u,v) = f_u*(phermone(u,v)/(p_u))
+        if (u,v) == d:
+            flow[(u,v)] = forward[(u-1,v)][(u,v)]+forward[(u+1)][(u,v)]+forward[(u,v-1)][(u,v)]+forward[(u,v+1)][(u,v)]
+            total_phermones = phermone[(u,v)][(u+1,v)]+phermone[(u,v)][(u-1,v)]+phermone[(u,v)][(u,v+1)]+phermone[(u,v)][(u,v-1)]
+
+            forward[(u,v)][(u+1,v)] = flow[(u,v)]*(phermone[(u,v)][(u+1,v)]/total_phermones)
+            backward[(u+1,v)][(u,v)] = forward[(u,v)][(u+1,v)]
+            forward[(u,v)][(u-1,v)] = flow[(u,v)]*(phermone[(u,v)][(u-1,v)]/total_phermones)
+            backward[(u,v)][(u-1,v)] = forward[(u,v)][(u-1,v)]
+            forward[(u,v)][(u,v+1)] = flow[(u,v)]*(phermone[(u,v)][(u,v+1)]/total_phermones)
+            backward[(u,v)][(u,v+1)] = forward[(u,v)][(u,v+1)]
+            forward[(u,v)][(u,v-1)] = flow[(u,v)]*(phermone[(u,v)][(u,v-1)]/total_phermones)
+            backward[(u,v)][(u,v-1)] = forward[(u,v)][(u,v-1)]
           
 N = 10
 source = [1,2]
 base_flow_amount = 15
 phermone = graph.pheromone(N)
-flow = create_flow(N, source, base_flow_amount)
+flow = create_flow(True, N, source, base_flow_amount)
 update_flow(flow, phermone, N)
